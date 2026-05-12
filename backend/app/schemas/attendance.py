cf14023d18c8c0dd534d029ce5829e50e4453472
anthonimasaga89-code <anthonimasaga89@gmail.com>
2026-05-12 06:38:21 +0200

    this is what done today

--- backend/app/schemas/attendance.py
+++ backend/app/schemas/attendance.py
@@ -1,2 +1,14 @@
 from pydantic import BaseModel,EmailStr
 from typing import Optional
+
+class attendanceCreate(BaseModel):
+    employee_id:int
+    date:date
+
+class attendanceResponse(BaseModel):
+    id : int
+    employee_id:int
+    date:date
+    
+    class config:
+        from_attributes = True

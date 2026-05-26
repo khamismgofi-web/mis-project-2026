caf143998606d1544c544eeb56cb903127b9f6c9
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-26 13:21:02 +0300

    fix:fixing import

--- backend/app/models/Report.py
+++ backend/app/models/Report.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy.orm import Mapped,mapped_column,relationship
 from sqlalchemy import Column,Interger,String,Float,ForeignKey
 from app.core.database import Base
 

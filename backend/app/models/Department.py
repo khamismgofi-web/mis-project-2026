4138918b51dc52fba4636e101a5df19362d5f56d
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-26 13:28:34 +0300

    fix:fixing import error

--- backend/app/models/Department.py
+++ backend/app/models/Department.py
@@ -1,10 +1,10 @@
-from sqlalchemy.orm import Mapped,mapped_column
-from sqlalchemy import Column,Interger,String,Float,ForeignKey
+from sqlalchemy.orm import Mapped,mapped_column,relationship
+from sqlalchemy import Column,Interger,Float,ForeignKey,String
 from app.core.database import Base
 
 class department(Base):
     __tablename__ = "department"
     id:Mapped[int]= mapped_column(primary_key=True)
-    name:Mapped[str]=mapped_column(String_(100))
+    name:Mapped[str]=mapped_column(String(100),nullable=False)
 
     employees:Mapped[list["Employee"]]=relationship(back_populates="department")

6eeedd30e892fd9743725d22e86bfaa112d74f81
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-26 13:16:07 +0300

    fix:fixing typo error

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,6 +4,6 @@ from app.core.database import Base
 class user(Base):
     __tablename__="user"
     id:Mapped[int]=mapped_column(primary_key=True,index=True)
-    name:Mapped[str]=mapped_column(string(100),unique=True,nullable=False)
-    email:Mapped[str]=mapped_column(string(150),unique=True, nullable=False)
-    password:Mapped[str]=mapped_column(string(255),nullable=False)
+    name:Mapped[str]=mapped_column(String(100),unique=True,nullable=False)
+    email:Mapped[str]=mapped_column(String(150),unique=True, nullable=False)
+    password:Mapped[str]=mapped_column(String(255),nullable=False)

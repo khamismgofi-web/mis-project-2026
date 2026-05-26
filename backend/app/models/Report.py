071ac5314891c6869e1324e48b565b271b4ed534
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-26 13:25:35 +0300

    fix:fixing column naming error

--- backend/app/models/Report.py
+++ backend/app/models/Report.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped,mapped_column,relationship
-from sqlalchemy import Column,Interger,String,Float,ForeignKey
+from sqlalchemy import Column,Interger,String,Float,ForeignKey,Text
 from app.core.database import Base
 
 
@@ -7,7 +7,7 @@ class Report(Base):
     __tablename__="Report"
     id:Mapped[int]=mapped_column(primary_key=True, index=True)
     title:Mapped[str]=mapped_column(String(200),nullable=False)
-    desciption:Mapped[str]=mapped_column(ForeignKey("employees.id"))
+    desciption:Mapped[str]=mapped_column(Text,nullable=True)
 
 #relationship
     employee_id:Mapped["Employee"]=relationship(back_populates="reports")

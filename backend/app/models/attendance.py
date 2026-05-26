7416dc3503b97f5ebf928ec5f153ee4806ccaa13
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-26 13:33:46 +0300

    fix:fixing import error,empty class table error and indetation error

--- backend/app/models/attendance.py
+++ backend/app/models/attendance.py
@@ -1,14 +1,15 @@
-from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy.orm import Mapped,mapped_column,relationship
 from sqlalchemy import Column,Interger,String,Float,ForeignKey
 from app.core.database import Base
 
 class Employee(Base):
-    __tablename__
+    __tablename__="Employee"
     id:Mapped[int]=mapped_column(primary_key=True)
-    name:Mapped[str]=mapped_column(String(100))
-    email:Mapped[str]=mapped_column(String(100))
+    name:Mapped[str]=mapped_column(String(100),nullable=False)
+    email:Mapped[str]=mapped_column(String(100),nullable=False)
     department_id:Mapped[int]=mapped_column(ForeignKey("department.id"))
-    department:Mapped["Department"]= relationship(back_populates="employees")
-    attendances:Mapped[list["Attendance"]]=relationship(back_populates = "employee")
+#Relationship
+department:Mapped["Department"]= relationship(back_populates="employees")
+attendances:Mapped[list["Attendance"]]=relationship(back_populates = "employee")
     
 

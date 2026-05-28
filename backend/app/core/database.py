d7c7eec16a3a2d358624b59d4eb8ca799dd6343b
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-28 17:50:49 +0300

    feat:setting declarative base

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -6,4 +6,6 @@ from sqlalchemy.orm import sessionmaker,declarative_base
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine=create_engine(DATABASE_URL)
-SessionLocal = sessionmaker
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base = declarative_base()

f6cebd25906fccb7fb5ef523e839c9df793e0236
Abdully <seniorkalima194@gmail.com>
2026-05-29 16:38:52 +0300

    added some code to model

--- model.py
+++ model.py
@@ -1,22 +1,54 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, Integer,  ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+from date import datetime
+
 
 Base = declarative_base()
 
 class Employee(Base):
-    __tablename__ = "employees"
+    __tablename__ = "Employees"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     email = Column(String(50), unique=True)
-    department = Column(String(25))
-    report = Column(String(100))
+    position = Column(String(25))
+    salary = Column(Integer)
+    department_name = Column(Integer, ForeignKey("department.name"))
 
 
 class Department(Base):
-    __tablename__ = "department"
+    __tablename__ = "Department"
 
-    department_name = Column(String, primary_key= True, unique=True)
+    
+    name = Column(String, primary_key= True, unique=True)
     head_of_department = Column(String, )
     activity = Column(String(100), nullable=False)
+
+class Attendance(Base):
+    __tablename__ = "Attendance"
+
+    id = Column(String(50), primary_key= True)
+    Employee_name = Column(String(80),ForeignKey = "Employee.id", nullable = False)
+    date = Column(Date, nullable = False)
+    status = Column(String(100))
+    check_in = Column(Datetime)
+    check_out = Column(Datetime)
+
+class Report(Base):
+    __tablename__ = "reports"
+
+    id = Column(Integer, nullable=False, primary_key= True)
+    Employee_name = Column(String(100), nullable = False, ForeignKey = "employee.id")
+    position = Column (String(100), nullable = False, ForeignKey = "Employee.position")
+    description = Column(String(300), nullable= True)
+    created_at = Column(Datetime, default= datetime.utcnow)
+
+class user(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    username = Column(String(100), unique= True, nullable = False)
+    pasword = Column(String(25), nullable = False, unique = True)
+    role = column(sring(100), unique = True)
+

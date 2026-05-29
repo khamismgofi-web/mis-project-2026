725abbdd13b1f6c1bb0b9af1087983e87141a4e0
khamismgofi-web <khamismgofi18@gmail.com>
2026-05-29 09:09:46 +0300

    feat:add column to user table

--- backend/app/models/attendance.py
+++ backend/app/models/attendance.py
@@ -1,3 +1,5 @@
 from sqlalchemy.orm import Mapped,mapped_column
 from sqlalchemy import Column,Interger,String,Float,ForeignKey
 from app.core.database import Base
+
+class attendance
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Mapped,mapped_column
-from sqlalchemy import Column,Interger,String,Float,ForeignKey,uuid
+from sqlalchemy import Interger,String,Float,Boolean,ForeignKey,DateTime,func
 from app.core.database import Base
-import uuid
+import uuid,enum
 
 class USerRole(str,enum.Enum):
     adimn="ADIMN"
@@ -11,4 +11,7 @@ class User(Base):
     __tablename__="users"
 
     id:Mapped[uuid.UUID] = mapped_column(Interger,primary_key=True,index=True)
-    email:Mapped[str]= Column(Str)
+    email:Mapped[str]= mapped_column(String(200),unique=True,nullable=False,index=True)
+    password_hash:Mapped[str] = mapped_column(String(120),nullable=False)
+    is_active:Mapped[bool] = mapped_column(Boolean,default=True)
+    created_at:Mapped[str] = mapped_column(DateTime(timezone=True),server_default=func.now())

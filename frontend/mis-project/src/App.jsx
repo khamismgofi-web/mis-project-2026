192a70f41f9c6c1391ea5e77eb0a4118fdda69ed
khamismgofi-web <khamismgofi18@gmail.com>
2026-06-03 08:27:14 +0300

    feat:make proper project path file structure

--- frontend/mis-project/src/App.jsx
+++ frontend/mis-project/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
 
 function App() {
   const title = 'Employee Management System';
   return (
-    <div>
-      <h1>Welcome {title}</h1>
+    <div className="text-3xl text-teal-500 font-bold underline"> Welcome {title}
       
     </div>
   );
